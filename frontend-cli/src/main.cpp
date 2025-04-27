c1f63b94e1a0e94f08c2e64f81319e17520914a7
landiluigi746 <landiluigi865@gmail.com>
2025-04-27 23:10:17 +0200

    [init]: Frontend-cli: Add cpp-httplib dependency with simple test

--- frontend-cli/src/main.cpp
+++ frontend-cli/src/main.cpp
@@ -1,26 +1,39 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 
+#include <httplib.h>
+#include <print>
+
 int main()
 {
-    using namespace ftxui;
+    // using namespace ftxui;
 
-    auto document = vbox({
-        hbox({
-            text("one") | border,
-            text("two") | border | flex,
-            text("three") | border | flex,
-        }),
+    // auto document = vbox({
+    //     hbox({
+    //         text("one") | border,
+    //         text("two") | border | flex,
+    //         text("three") | border | flex,
+    //     }),
 
-        gauge(0.25) | color(Color::Red),
-        gauge(0.50) | color(Color::White),
-        gauge(0.75) | color(Color::Blue),
-    });
+    //     gauge(0.25) | color(Color::Red),
+    //     gauge(0.50) | color(Color::White),
+    //     gauge(0.75) | color(Color::Blue),
+    // });
 
-    auto screen = Screen::Create(Dimension::Fit(document));
-    Render(screen, document);
+    // auto screen = Screen::Create(Dimension::Fit(document));
+    // Render(screen, document);
 
-    screen.Print();
+    // screen.Print();
+
+    httplib::Client cli("localhost", 8000);
+
+    if (const auto res = cli.Get("/"))
+    {
+        std::println("Status: {}", res->status);
+        std::println("Response body: {}", res->body);
+    }
+    else
+        std::println("Status: {}", res->status);
 
     return 0;
 }
